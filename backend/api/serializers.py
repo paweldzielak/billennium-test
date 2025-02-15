d72877ebbe9e2f4449d19a1fd934e93c91a8e8c1
Paweł Dziełak <pawel.programowanie@hotmail.com>
2025-02-15 10:55:49 +0100

    backend with notes and JWT auth + gitignore

---
+++ backend/api/serializers.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Note
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}  # do not send password back
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        print('UserSerializer.Meta.create user=', user)
+        return user
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        # TODO: check if is there any way to take all by default, and not repeat code for specifying fields
+        fields = ['id', 'title', 'content', 'created_at', 'author'],
+        extra_kwargs = {'author': {'read_only': True}}
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .serializers import UserSerializer, NoteSerializer
+from .models import Note
 
-# Create your views here.
+
+class NoteListCreate(generics.ListAPIView):  # automatic view creation handle - by django
+    # queryset = Note.objects.all()     # not valid for lists - use get_queryset!
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]  # we cannot reach this route unless we are authenticated
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+    def perform_create(self, serializer):  # is this name fixed?
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]  # we cannot reach this route unless we are authenticated
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+
+class CreateUserView(generics.CreateAPIView):  # automatic view creation handle - by django
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
